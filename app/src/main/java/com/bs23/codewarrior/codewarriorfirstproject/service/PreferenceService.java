29eeac38b6fd065f45459b2cb701a965fdee8442
Mun Mun Das <munmun.das@bs-23.com>
2015-01-21 00:10:32 +0600

    auth preferences are now saved preferences service

--- app/src/main/java/com/bs23/codewarrior/codewarriorfirstproject/service/PreferenceService.java
+++ app/src/main/java/com/bs23/codewarrior/codewarriorfirstproject/service/PreferenceService.java
@@ -2,11 +2,16 @@ package com.bs23.codewarrior.codewarriorfirstproject.service;
 
 import android.content.SharedPreferences;
 
+import com.bs23.codewarrior.codewarriorfirstproject.model.LoginResponse;
+
 import javax.inject.Inject;
 
 public class PreferenceService {
-    public static final String SHARED_PREF_KEY = "com.bs23.codewarrior.codewarriorfirstproject.service" ;
-    public static final String ACCESS_TOKEN = "access_token" ;
+    public static final String SHARED_PREF_KEY = "com.bs23.codewarrior.codewarriorfirstproject.service";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String TOKEN_TYPE = "token_type";
+    public static final String USER_EMAIL = "user_email";
+
     @Inject
     private SharedPreferences preferences;
 
@@ -18,5 +23,11 @@ public class PreferenceService {
     public void SetPreferenceValue(String key, String value)
     {
         preferences.edit().putString(key, value).apply();
+    }
+
+    public void SaveAuthPreferences(LoginResponse response)
+    {
+        preferences.edit().putString(ACCESS_TOKEN, response.access_token);
+        preferences.edit().putString(TOKEN_TYPE, response.token_type);
     }
 }
