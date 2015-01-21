2ac04f85a57bc30c074b77c652ccc4b822621e15
ovi0606 <ovi0606@gmail.com>
2015-01-21 22:15:56 +0600

    fix

--- app/src/main/java/com/bs23/codewarrior/codewarriorfirstproject/DashboardActivity.java
+++ app/src/main/java/com/bs23/codewarrior/codewarriorfirstproject/DashboardActivity.java
@@ -1,25 +1,128 @@
 package com.bs23.codewarrior.codewarriorfirstproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.bs23.codewarrior.codewarriorfirstproject.model.UserProfile;
+import com.bs23.codewarrior.codewarriorfirstproject.service.AuthService;
+import com.bs23.codewarrior.codewarriorfirstproject.service.PreferenceService;
+import com.google.inject.Inject;
 
-public class DashboardActivity extends ActionBarActivity {
+import java.util.concurrent.Callable;
 
+import javax.security.auth.callback.Callback;
+
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+@ContentView(R.layout.activity_dash_board)
+public class DashBoardActivity extends RoboActivity {
+
+    @Inject
+    PreferenceService preferenceService;
+    
+    @Inject
+    private AuthService authService;
+    
+    @InjectView (R.id.buttonEditProfile)
+    private Button buttonEditProfile;
+    
+    @InjectView (R.id.buttonShare)
+    private Button buttonShare;
+    
+    @InjectView (R.id.buttonLogout)
+    private Button buttonLogout;
+    
+    private UserProfile userProfileData;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        getSupportActionBar().setTitle("Dashboard");
+        
+        if(!preferenceService.isSignedIn(PreferenceService.IS_SIGNIN)){
+            
+            startActivity(new Intent(DashBoardActivity.this, SignInActivity.class));
+            
+        }
+        getUserData();
+        
+        buttonEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashBoardActivity.this, UpdateProfileActivity.class));
+            }
+        });
+        
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareThis();
+            }
+        });
+        
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preferenceService.ResetAuthPreferences();
+                startActivity(new Intent(DashBoardActivity.this, SignInActivity.class));
+            }
+        });
+        
+        
     }
 
+    private void shareThis() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is simple Share");
+        startActivity(Intent.createChooser(shareIntent, "Share"));
+    }
+
+    private void getUserData() {
+        authService.getProfile(new retrofit.Callback<UserProfile>() {
+            @Override
+            public void success(UserProfile userProfile, Response response) {
+                userProfileData = userProfile;
+                
+                preferenceService.SetPreferenceValue(PreferenceService.ID,String.valueOf(userProfileData.getId()));
+                
+                /*if(isStringIsNull(userProfile.getFirstName())||isStringIsNull(userProfile.getFirstName())||isStringIsNull(userProfile.getFirstName())||isStringIsNull(userProfile.getFirstName())){
+                    //buttonEditProfile.setVisibility(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(),"Please update your profile Info",Toast.LENGTH_LONG).show();
+                }*/
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+    }
+    
+    private boolean isStringIsNull(String string){
+        if(string != null && !string.isEmpty()){
+            
+            return false;
+        } 
+        return true;
+        
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        getMenuInflater().inflate(R.menu.menu_dash_board, menu);
         return true;
     }
 
